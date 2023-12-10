9a2a10d29a1dfe6905d522567cdfda1cffb7d213
dazzul94 <dazzul94@gmail.com>
2023-12-10 14:56:58 +0900

    hello job

---
+++ ds-batch-sample/src/main/java/com/ds/sample/DsBatchSampleApplication.java
@@ -0,0 +1,33 @@
+package com.ds.sample;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@SpringBootApplication
+@EnableBatchProcessing
+public class DsBatchSampleApplication {
+
+	public static void main(String[] args) {
+		// 스프링 배치가 정상적으로 종료될 수 있도록 System.exit(SpringApplication.exit();
+		System.exit(SpringApplication.exit(SpringApplication.run(DsBatchSampleApplication.class, args)));
+	}
+
+	/**
+	 * // TaskExecutor가 기본적으로 Bean으로 생성되어 있기 때문에 기본 @Bean으로 사용하기 위함을 표시
+	 */
+	@Bean
+	@Primary
+	TaskExecutor taskExecutor() {
+		ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+		taskExecutor.setCorePoolSize(10); // 쓰레드 기본 사이즈 10
+		taskExecutor.setMaxPoolSize(20); // 최대 쓰레드 사이즈 20
+		taskExecutor.setThreadNamePrefix("batch-thread-"); // 배치 쓰레드 Prefix 이름이 찍히게 됨.
+		taskExecutor.initialize();
+		return taskExecutor;
+	}
+}
---
+++ ds-batch-sample/src/test/java/com/ds/sample/DsBatchSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsBatchSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
